99bb41da938637747d730ca8f7dc181ff6519a76
Aitor Pagan <aitor.pagan@apiumhub.com>
2025-12-04 15:48:59 +0100

    Day2

--- src/Utils.kt
+++ src/Utils.kt
@@ -18,4 +18,5 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
 /**
  * The cleaner shorthand for printing output.
  */
-fun Any?.println() = kotlin.io.println(this)
+fun Any?.println() = println(this)
+
